49d2a7430d9d11885c64a4031305657202fc94e5
liuqixing <liuqixing@dekuan.org>
2019-05-16 05:37:42 +0800

    released version 0.4.8

--- src/calendar.module.ts
+++ src/calendar.module.ts
@@ -1,20 +1,28 @@
-import { NgModule }      from '@angular/core';
-import { IonicModule } from 'ionic-angular';
+import {NgModule} from '@angular/core';
+import {IonicModule} from 'ionic-angular';
 import 'intl';
 import 'intl/locale-data/jsonp/en';
-import { MonthViewComponent } from './monthview';
-import { WeekViewComponent } from './weekview';
-import { DayViewComponent } from './dayview';
+import {MonthViewComponent} from './monthview';
+import {WeekViewComponent} from './weekview';
+import {DayViewComponent} from './dayview';
 import {CalendarComponent} from './calendar';
-import { CalendarService } from './calendar.service';
-import { initPositionScrollComponent } from './init-position-scroll';
+import {CalendarService} from './calendar.service';
+import {initPositionScrollComponent} from './init-position-scroll';
+
 
 @NgModule({
-    declarations: [
-        MonthViewComponent, WeekViewComponent, DayViewComponent, CalendarComponent, initPositionScrollComponent
-    ],
-    imports: [IonicModule],
-    exports: [CalendarComponent],
-    entryComponents: [CalendarComponent]
+	declarations:
+	[
+		MonthViewComponent,
+		WeekViewComponent,
+		DayViewComponent,
+		CalendarComponent,
+		initPositionScrollComponent
+	],
+	imports: [IonicModule],
+	exports: [CalendarComponent],
+	entryComponents: [CalendarComponent]
 })
-export class NgCalendarModule {}
+export class NgCalendarModule
+{
+}
